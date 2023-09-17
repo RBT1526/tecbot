efb914476ff9f9566f25d8e79dfc9d09ebe4f442
Gerardo Fregoso Jimenez <gerardofregosojimenez@Gerardos-MacBook-Pro-4.local>
2023-09-16 22:55:48 -0600

    Gyroscope motor updated

--- PRUEBAS/prueba_gyroscope_motor/prueba_gyroscope_motor.ino
+++ PRUEBAS/prueba_gyroscope_motor/prueba_gyroscope_motor.ino
@@ -7,8 +7,13 @@
  * some of the MPU6050-raw-data-extraction code in void loop() and most in calculateError() are written by Dejan from:
  * https://howtomechatronics.com/tutorials/arduino/arduino-and-mpu6050-accelerometer-and-gyroscope-tutorial/
 */
-#include "CurieIMU.h"
-//control pins for left and right motors
+#include <CurieIMU.h>
+#include <MadgwickAHRS.h>
+
+Madgwick filter;
+unsigned long microsPerReading, microsPrevious;
+float accelScale, gyroScale;
+
 const int leftSpeed = 3; //means pin 9 on the Arduino controls the speed of left motor
 const int rightSpeed = 6;
 const int left1 = 5; //left 1 and left 2 control the direction of rotation of left motor
@@ -16,19 +21,13 @@ const int left2 = 4;
 const int right1 = 7;
 const int right2 = 8;
 
-float AccX, AccY, AccZ; //linear acceleration
-float GyroX, GyroY, GyroZ; //angular velocity
-float accAngleX, accAngleY, gyroAngleX, gyroAngleY, gyroAngleZ; //used in void loop()
-float roll, pitch, yaw;
-float AccErrorX, AccErrorY, GyroErrorX, GyroErrorY, GyroErrorZ;
-float elapsedTime, currentTime, previousTime;
-int c = 0;
+
 
 const int maxSpeed = 200; //max PWM value written to motor speed pin. It is typically 255.
 const int minSpeed = 100; //min PWM value at which motor moves
 float angle; //due to how I orientated my MPU6050 on my car, angle = roll
 float targetAngle = 0;
-int equilibriumSpeed = 248; //rough estimate of PWM at the speed pin of the stronger motor, while driving straight 
+int equilibriumSpeed = 200; //rough estimate of PWM at the speed pin of the stronger motor, while driving straight 
 //and weaker motor at maxSpeed
 int leftSpeedVal;
 int rightSpeedVal;
@@ -37,9 +36,8 @@ bool prevIsDriving = true; //equals isDriving in the previous iteration of void 
 bool paused = false; //is the program paused
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   // Call this function if you need to get the IMU error values for your module
-  calculateError();
   delay(20);
   pinMode(left1, OUTPUT);
   pinMode(left2, OUTPUT);
@@ -47,33 +45,96 @@ void setup() {
   pinMode(right2, OUTPUT);
   pinMode(leftSpeed, OUTPUT);
   pinMode(rightSpeed, OUTPUT);
-  currentTime = micros();
+
+  CurieIMU.begin();
+
+
+  CurieIMU.setGyroRate(25);
+
+  CurieIMU.setAccelerometerRate(25);
+
+  filter.begin(25);
+
+  // Set the accelerometer range to 2G
+
+  CurieIMU.setAccelerometerRange(2);
+
+  // Set the gyroscope range to 250 degrees/second
+
+  CurieIMU.setGyroRange(250);
+
+  // initialize variables to pace updates to correct rate
+
+  microsPerReading = 1000000 / 25;
+
+  microsPrevious = micros();
 }
 
 void loop() {
   // === Read accelerometer (on the MPU6050) data === //
-  readAcceleration();
-  // Calculating Roll and Pitch from the accelerometer data
-  accAngleX = (atan(AccY / sqrt(pow(AccX, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorX; //AccErrorX is calculated in the calculateError() function
-  accAngleY = (atan(-1 * AccX / sqrt(pow(AccY, 2) + pow(AccZ, 2))) * 180 / PI) - AccErrorY;
-  
-  // === Read gyroscope (on the MPU6050) data === //
-  previousTime = currentTime;
-  currentTime = micros();
-  elapsedTime = (currentTime - previousTime) / 1000000; // Divide by 1000 to get seconds
-  readGyro();
-  // Correct the outputs with the calculated error values
-  GyroX -= GyroErrorX; //GyroErrorX is calculated in the calculateError() function
-  GyroY -= GyroErrorY;
-  GyroZ -= GyroErrorZ;
-  // Currently the raw values are in degrees per seconds, deg/s, so we need to multiply by sendonds (s) to get the angle in degrees
-  gyroAngleX += GyroX * elapsedTime; // deg/s * s = deg
-  gyroAngleY += GyroY * elapsedTime;
-  yaw += GyroZ * elapsedTime;
-  //combine accelerometer- and gyro-estimated angle values. 0.96 and 0.04 values are determined through trial and error by other people
-  roll = 0.96 * gyroAngleX + 0.04 * accAngleX;
-  pitch = 0.96 * gyroAngleY + 0.04 * accAngleY;
-  angle = roll; //if you mounted MPU6050 in a different orientation to me, angle may not = roll. It can roll, pitch, yaw or minus version of the three
+  int aix, aiy, aiz;
+
+  int gix, giy, giz;
+
+  float ax, ay, az;
+
+  float gx, gy, gz;
+
+  float roll, pitch, heading;
+
+  unsigned long microsNow;
+  microsNow = micros();
+
+  if (microsNow - microsPrevious >= microsPerReading) {
+
+    // read raw data from CurieIMU
+
+    CurieIMU.readMotionSensor(aix, aiy, aiz, gix, giy, giz);
+
+    // convert from raw data to gravity and degrees/second units
+
+    ax = convertRawAcceleration(aix);
+
+    ay = convertRawAcceleration(aiy);
+
+    az = convertRawAcceleration(aiz);
+
+    gx = convertRawGyro(gix);
+
+    gy = convertRawGyro(giy);
+
+    gz = convertRawGyro(giz);
+
+    // update the filter, which computes orientation
+
+    filter.updateIMU(gx, gy, gz, ax, ay, az);
+
+    // print the heading, pitch and roll
+
+    roll = filter.getRoll();
+
+    pitch = filter.getPitch();
+
+    heading = filter.getYaw();
+
+    Serial.print("Orientation: ");
+
+    Serial.print(heading);
+
+    Serial.print(" ");
+
+    Serial.print(pitch);
+
+    Serial.print(" ");
+
+    Serial.println(roll);
+
+    // increment previous time, so we keep proper pace
+
+    microsPrevious = microsPrevious + microsPerReading;
+
+  }
+  angle = heading; //if you mounted MPU6050 in a different orientation to me, angle may not = roll. It can roll, pitch, yaw or minus version of the three
   //for me, turning right reduces angle. Turning left increases angle.
   
   // Print the values on the serial monitor
@@ -164,6 +225,36 @@ void driving (){//called by void loop(), which isDriving = true
   }
 }
 
+float convertRawAcceleration(int aRaw) {
+
+  // since we are using 2G range
+
+  // -2g maps to a raw value of -32768
+
+  // +2g maps to a raw value of 32767
+
+
+
+  float a = (aRaw * 2.0) / 32768.0;
+
+  return a;
+}
+
+float convertRawGyro(int gRaw) {
+
+  // since we are using 250 degrees/seconds range
+
+  // -250 maps to a raw value of -32768
+
+  // +250 maps to a raw value of 32767
+
+
+
+  float g = (gRaw * 250.0) / 32768.0;
+
+  return g;
+}
+
 void controlSpeed (){//this function is called by driving ()
   int deltaAngle = round(targetAngle - angle);
   int targetGyroX;
@@ -228,59 +319,6 @@ int changeSpeed (int motorSpeed, int increment){
   return motorSpeed;
 }
 
-void calculateError() {
-  //When this function is called, ensure the car is stationary. See Step 2 for more info
-  
-  // Read accelerometer values 200 times
-  c = 0;
-  while (c < 200) {
-    readAcceleration();
-    // Sum all readings
-    AccErrorX += (atan((AccY) / sqrt(pow((AccX), 2) + pow((AccZ), 2))) * 180 / PI);
-    AccErrorY += (atan(-1 * (AccX) / sqrt(pow((AccY), 2) + pow((AccZ), 2))) * 180 / PI);
-    c++;
-  }
-  //Divide the sum by 200 to get the error value, since expected value of reading is zero
-  AccErrorX = AccErrorX / 200;
-  AccErrorY = AccErrorY / 200;
-  c = 0;
-  
-  // Read gyro values 200 times
-  while (c < 200) {
-    readGyro();
-    // Sum all readings
-    GyroErrorX += GyroX;
-    GyroErrorY += GyroY;
-    GyroErrorZ += GyroZ;
-    c++;
-  }
-  //Divide the sum by 200 to get the error value
-  GyroErrorX = GyroErrorX / 200;
-  GyroErrorY = GyroErrorY / 200;
-  GyroErrorZ = GyroErrorZ / 200;
-  Serial.println("The the gryoscope setting in MPU6050 has been calibrated");
-}
-
-void readAcceleration() {
-  Wire.beginTransmission(MPU);
-  Wire.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-  //For a range of +-2g, we need to divide the raw values by 16384, according to the MPU6050 datasheet
-  AccX = (Wire.read() << 8 | Wire.read()) / 16384.0; // X-axis value
-  AccY = (Wire.read() << 8 | Wire.read()) / 16384.0; // Y-axis value
-  AccZ = (Wire.read() << 8 | Wire.read()) / 16384.0; // Z-axis value
-}
-
-void readGyro() {
-  Wire.beginTransmission(MPU);
-  Wire.write(0x43);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 6, true);
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
-}
 
 void stopCar(){
   digitalWrite(right1, LOW);
