e9adc3bca22f33fb345bb7002bf52c124e4c0b75
Gerardo Fregoso Jimenez <gerardofregosojimenez@Gerardos-MacBook-Pro-4.local>
2023-09-23 13:23:43 -0600

    Prueba_motor_sens added y modified prueba_motor

--- PRUEBAS/prueba_motor_sensor/prueba_motor_sensor.ino
+++ PRUEBAS/prueba_motor_sensor/prueba_motor_sensor.ino
@@ -5,6 +5,12 @@ const int pwm_b = 3;
 const int izq_a = 5;
 const int izq_b = 4;
 
+bool flag = false;
+
+int getdist(int s){
+    int dist= pow(10,log10(s/1821.2)/-0.65);
+    return dist;
+}
 void setup (){
 pinMode(pwm_a, OUTPUT);
 pinMode(der_a, OUTPUT);
@@ -16,34 +22,33 @@ Serial.begin(115200);
 
 }
 void loop(){
+int dist = getdist(analogRead(A5));
+if (dist > 10 && flag == false){
 analogWrite(pwm_a,150);
 digitalWrite(der_a,HIGH);
 digitalWrite(der_b,LOW);
 analogWrite(pwm_b,110);
 digitalWrite(izq_a,HIGH);
 digitalWrite(izq_b,LOW);
-delay(2000);
+}
+else if (dist <= 10 && flag == false){
 analogWrite(pwm_a,150);
 digitalWrite(der_a,HIGH);
 digitalWrite(der_b,LOW);
-analogWrite(pwm_b,150);
+analogWrite(pwm_b,110);
 digitalWrite(izq_a,LOW);
 digitalWrite(izq_b,HIGH);
-delay(2000);
+delay(750);
 analogWrite(pwm_a,150);
-digitalWrite(der_a,LOW);
-digitalWrite(der_b,HIGH);
-analogWrite(pwm_b,150);
-digitalWrite(izq_a,LOW);
-digitalWrite(izq_b,HIGH);
-delay(2000);
-analogWrite(pwm_a,150);
-digitalWrite(der_a,LOW);
-digitalWrite(der_b,HIGH);
-analogWrite(pwm_b,150);
+digitalWrite(der_a,HIGH);
+digitalWrite(der_b,LOW);
+analogWrite(pwm_b,110);
 digitalWrite(izq_a,HIGH);
 digitalWrite(izq_b,LOW);
-delay(2000);
+
+}
+
+
 /*
 delay(2000);
 analogWrite(pwm_a,150);
--- PRUEBAS/prueba_sensor_dist/prueba_sensor_dist.ino
+++ PRUEBAS/prueba_sensor_dist/prueba_sensor_dist.ino
@@ -2,7 +2,7 @@ void setup(){
     Serial.begin(115200);
 }
 void loop(){
-    int s = analogRead(A0);
+    int s = analogRead(A5);
     int dist= pow(10,log10(s/1821.2)/-0.65);
     Serial.println(dist);
     delay(100);
