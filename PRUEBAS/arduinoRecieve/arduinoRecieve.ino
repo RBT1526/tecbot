9763153e556185316dfea8d65e90d4dc6c7bfebc
GilbertoMM27 <gil.malagamba@gmail.com>
2023-09-27 21:17:26 -0600

    libreria serialUtil jala

--- PRUEBAS/arduinoRecieve/arduinoRecieve.ino
+++ PRUEBAS/arduinoRecieve/arduinoRecieve.ino
@@ -1,15 +1,67 @@
-char Mymessage[10]; //Initialized variable to store recieved data
-  
-  void setup() {
-    // Begin the Serial at 9600 Baud
-    Serial.begin(9600);
+#include "SerialUtil.h"
+
+SerialUtil mySerial;
+
+bool valueBool;
+int valueInt;
+float valueFloat=NAN;
+String valueStr;
+
+void setup() {
+  Serial.begin(9600);
+  // Begin the Serial at 9600 Baud
+  mySerial.setBaudrate(9600);
+  mySerial.changeWaitTime(20);
+  mySerial.startMode(MODE_RECIEVE);
+  mySerial.comType(true,true);
+  mySerial.attachSend(sendMyData);
+  mySerial.attachRecieve(recieveMyData);
+  mySerial.setTimeOut(2000);
+  mySerial.setTimeChangeCom(5000);
+  Serial.println("End setup");
+}
+
+void loop() {
+  if(isnan(valueFloat)){
+    Serial.print("aun no ");
+    mySerial.loop(millis()); 
+    recieveMyData('A');
+  }else{
+    Serial.print("fin ");
+    Serial.println(valueFloat);
   }
-  
-  void loop() { 
-    Serial.readBytes(Mymessage,5); //Read the serial data and store in var
-    if (Mymessage[0] == 'H'){
-      Serial.println("llego h");
-    }//Print data on Serial Monitor
-    delay(1000);
+}
+
+void sendMyData(){
+  //Time to send data    
+  mySerial.sendFloat('a', 15.6);
+  mySerial.sendFloat('b', 16.7);
+  mySerial.sendFloat('c', 17.8);
+  mySerial.sendInt('k', 10);
+  mySerial.sendBool('p', false);
+  mySerial.sendText('t', "Dit is een test van B naar A", 50);
+}
+
+void recieveMyData(int sendCode){  
+  if (sendCode == 'A' || sendCode == 'B' || sendCode == 'C'
+      || sendCode == 'D' || sendCode == 'E' || sendCode == 'F'
+      || sendCode == 'G' || sendCode == 'H' || sendCode == 'I'
+      || sendCode == 'J') {
+    valueFloat = mySerial.readFloat();
+    Serial.println(valueFloat);
   }
-  //buenas noches cabron -G
+  else if (sendCode == 'K' || sendCode == 'L' || sendCode == 'M'
+           || sendCode == 'N' || sendCode == 'O') {
+    valueInt = mySerial.readInt();
+    Serial.println(valueInt);
+  }
+  else if (sendCode == 'P' || sendCode == 'Q' || sendCode == 'R'
+           || sendCode == 'S') {
+    valueBool = mySerial.readBool();
+    Serial.println(valueBool);
+  }
+  else if (sendCode == 'T') {
+    valueStr = mySerial.readText(50);
+    Serial.println(valueStr);
+  }
+}
--- PRUEBAS/arduinoSend/arduinoSend.ino
+++ PRUEBAS/arduinoSend/arduinoSend.ino
@@ -1,20 +1,74 @@
-char Mymessage[5] = {'H','e','l','l','o'}; //String data
- 
-  void setup() {
- 
-    // Begin the Serial at 9600 Baud
- 
-    Serial.begin(9600);
- 
+#include "SerialUtil.h"
+
+SerialUtil mySerial;
+
+bool valueBool;
+int valueInt;
+float valueFloat;
+String valueStr;
+
+void setup() {
+  Serial.begin(9600);
+  // Begin the Serial at 9600 Baud
+  mySerial.setBaudrate(9600);
+  mySerial.changeWaitTime(20);
+  mySerial.startMode(MODE_SEND);
+  mySerial.comType(true,true);
+  mySerial.attachSend(sendMyData);
+  mySerial.attachRecieve(recieveMyData);
+  mySerial.setTimeOut(2000);
+  mySerial.setTimeChangeCom(5000);
+  Serial.println("End setup");
+}
+
+void loop() {
+  mySerial.loop(millis()); 
+  sendMyData();
+}
+
+void sendMyData(){
+  //Time to send data    
+  mySerial.sendFloat('A', 20.6);
+  /*mySerial.sendFloat('B', 10.8);
+  mySerial.sendFloat('C', 123.9);
+  mySerial.sendFloat('D', 325.0);
+  mySerial.sendFloat('E', 129.2);
+  mySerial.sendFloat('F', 35.2);
+  mySerial.sendFloat('G', 67.9);
+  mySerial.sendFloat('H', 23.1);
+  mySerial.sendFloat('I', 1.1);
+  mySerial.sendFloat('J', 21.7);
+
+  mySerial.sendInt('K', 10);
+  mySerial.sendInt('L', 20);
+  mySerial.sendInt('M', 30);
+  mySerial.sendInt('N', 40);
+  mySerial.sendInt('O', 50);
+
+  mySerial.sendBool('P', false);
+  mySerial.sendBool('Q', true);
+  mySerial.sendBool('R', false);
+  mySerial.sendBool('S', true);
+
+  mySerial.sendText('T', "Dit is een test van A naar B", 50);
+  mySerial.sendText('T', "Dit is nog een andere test", 50);*/
+}
+
+void recieveMyData(int sendCode){
+  if (sendCode == 'a' || sendCode == 'b' || sendCode == 'c') {
+    valueFloat = mySerial.readFloat();
+    Serial.println(valueFloat);
   }
- 
- 
- 
- 
-  void loop() {
- 
-    Serial.write(Mymessage,5); //Write the serial data
- 
-    delay(1000);
- 
+  else if (sendCode == 'k') {
+    valueInt = mySerial.readInt();
+    Serial.println(valueInt);
   }
+  else if (sendCode == 'p') {
+    valueBool = mySerial.readBool();
+    Serial.println(valueBool);
+  }
+  else if (sendCode == 't') {
+    valueStr = mySerial.readText(50);
+    Serial.println(valueStr);
+  }
+}
