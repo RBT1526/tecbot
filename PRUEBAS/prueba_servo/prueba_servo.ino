c0c530a68498bbca2b769e98305a2b74d83c073d
GilbertoMM27 <gil.malagamba@gmail.com>
2023-09-28 20:15:58 -0600

    pullin

--- PRUEBAS/prueba_servo/prueba_servo.ino
+++ PRUEBAS/prueba_servo/prueba_servo.ino
@@ -1,26 +1,22 @@
-/*
- Controlling a servo position using a potentiometer (variable resistor)
- by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
-
- modified on 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Knob
-*/
-
 #include <Servo.h>
-
-Servo myservo;  // create servo object to control a servo
-
-int potpin = A0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
-
+Servo servo;
+int angle = 0;
 void setup() {
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  servo.attach(8);
+  servo.write(angle);
 }
-
-void loop() {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it for use with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+void loop() 
+{ 
+ // scan from 0 to 180 degrees
+  for(angle = 0; angle < 180; angle++)  
+  {                                  
+    servo.write(angle);               
+    delay(15);                   
+  } 
+  // now scan back from 180 to 0 degrees
+  for(angle = 180; angle > 0; angle--)    
+  {                                
+    servo.write(angle);           
+    delay(15);       
+  } 
 }
